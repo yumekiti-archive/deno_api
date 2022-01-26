de3512000ff2ef6f840ed7318ad324f3e5062b71
yumekiti <yumekiti120440@gmail.com>
2022-01-26 16:29:53 +0900

    chenge: deps commentout

--- deno/deps.ts
+++ deno/deps.ts
@@ -3,7 +3,7 @@
  *
  * This module re-exports the required methods from the dependant remote Ramda module.
  */
- export {
-    add,
-    multiply,
-  } from "https://x.nest.land/ramda@0.27.0/source/index.js";
+//  export {
+//     add,
+//     multiply,
+//   } from "https://x.nest.land/ramda@0.27.0/source/index.js";
