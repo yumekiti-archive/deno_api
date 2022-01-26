07eba91924ed6f8cb1da2b7268838b83a449cc94
yumekiti <yumekiti120440@gmail.com>
2022-01-26 16:38:38 +0900

    chenge: port

--- deno/main.ts
+++ deno/main.ts
@@ -1,1 +1,9 @@
-console.log("Welcome to Deno!");
+// console.log("Welcome to Deno!");
+
+import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
+
+function handler(req: Request): Response {
+    return new Response("Hello, World!");
+}
+
+serve(handler, { port: 8080 });
