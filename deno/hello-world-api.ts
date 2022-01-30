695e9a6c304c8e0ba097fb46eb8570b687c31f03
yumekiti <yumekiti120440@gmail.com>
2022-01-30 23:09:15 +0900

    chenge: api

--- deno/hello-world-api.ts
+++ deno/hello-world-api.ts
@@ -2,10 +2,13 @@ import { serve } from './deps.ts';
 
 export default function(){
 
-    function handler(req: Request): Response {
-        return new Response("Hello, World!");
-    }
+    // function handler(req: Request): Response {
+    //     return new Response("Hello, World!");
+    // }
 
-    serve(handler, { port: 8080 });
+    // serve(handler, { port: 8080 });
+
+    console.log("http://localhost:8080/");
+    serve((req) => new Response("Hello World\n"), { port: 8080 });
 
 }
